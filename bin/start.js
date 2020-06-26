14492feeb543ecf41bee3fa99bd1e49d8029c347
Thomas Taylor <thomas.taylor@kineo.com>
2020-06-26 15:12:34 +0100

    Remove Heroku customisations

--- bin/start.js
+++ bin/start.js
@@ -1,13 +1,1 @@
-const uri = process.env.MONGODB_URI;
-if(uri) {
-  const [userDetails,rest] = uri.slice(10).split('@');
-  const [username,password] = userDetails.split(':');
-  const [hostDetails,dbname] = rest.split('/');
-  const [host,dbPort] = hostDetails.split(':');
-  process.env.ADAPT_AUTHORING_MONGODB__username = username;
-  process.env.ADAPT_AUTHORING_MONGODB__password = password;
-  process.env.ADAPT_AUTHORING_MONGODB__host = host;
-  process.env.ADAPT_AUTHORING_MONGODB__port = Number(dbPort);
-  process.env.ADAPT_AUTHORING_MONGODB__dbname = dbname;
-}
 require('adapt-authoring-core').App.instance;
