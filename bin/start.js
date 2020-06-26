374d4bed8cb3c4e596b1d3f97d09d131ed133cca
Thomas Taylor <thomas.taylor@kineo.com>
2020-06-26 13:53:34 +0100

    Add heroku changes

--- bin/start.js
+++ bin/start.js
@@ -1,1 +1,18 @@
+const port = process.env.PORT;
+const uri = process.env.MONGODB_URI;
+
+if(port) {
+  process.env.ADAPT_AUTHORING_SERVER__port = Number(port);
+}
+if(uri) {
+  const [userDetails,rest] = uri.slice(10).split('@');
+  const [username,password] = userDetails.split(':');
+  const [hostDetails,dbname] = rest.split('/');
+  const [host,dbPort] = hostDetails.split(':');
+  process.env.ADAPT_AUTHORING_MONGODB__username = username;
+  process.env.ADAPT_AUTHORING_MONGODB__password = password;
+  process.env.ADAPT_AUTHORING_MONGODB__host = host;
+  process.env.ADAPT_AUTHORING_MONGODB__port = Number(dbPort);
+  process.env.ADAPT_AUTHORING_MONGODB__dbname = dbname;
+}
 require('adapt-authoring-core').App.instance;
